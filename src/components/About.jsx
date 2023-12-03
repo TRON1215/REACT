0a030ffdbe3d523056b76a88d4a7db7c8cdde226
TRON1215 <aviv.livnat21@gmail.com>
2023-12-03 17:29:02 +0200

    tasks

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './About.css'
+
+export const About = () => {
+  return (
+    <div className='container'>
+      <h1 className='title'>Hi, my name is Aviv</h1>
+      <span className='description'>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis nisi iure ut rerum tenetur ea voluptas ab unde? Nulla, totam a obcaecati natus vitae quo? Repudiandae nihil corrupti porro nulla?
+      </span>
+    </div>
+  )
+}
